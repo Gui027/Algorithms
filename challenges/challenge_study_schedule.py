ded6ec5c33dbeffea54f3c1de81af7511db1352d
Guilherme <guilhermerg1234@gmail.com>
2022-09-03 14:35:28 -0300

    Req1 refactored

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,15 +1,14 @@
 def study_schedule(permanence_period, target_time):
     if target_time is None:
         return None
-    
+
     count = 0
-    
+
     for entrance, exit in permanence_period:
         if type(entrance) != int or type(exit) != int:
             return None
-        
+
         if target_time >= entrance and target_time <= exit:
             count += 1
-            
+
     return count
-        
