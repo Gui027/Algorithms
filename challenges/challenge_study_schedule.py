8f813ff423d341248a01d7ec850f875060f0a5b9
Guilherme <guilhermerg1234@gmail.com>
2022-09-03 14:32:09 -0300

    Req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    if target_time is None:
+        return None
+    
+    count = 0
+    
+    for entrance, exit in permanence_period:
+        if type(entrance) != int or type(exit) != int:
+            return None
+        
+        if target_time >= entrance and target_time <= exit:
+            count += 1
+            
+    return count
+        
