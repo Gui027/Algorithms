f3c9aa9cc6f71680b834e8cd266911c2c6c4c5a9
Guilherme <Guilhermer1234@gmail.com>
2022-09-05 14:21:00 -0300

    Req3 algum progresso

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,15 @@
+def contar(letra, palavra):
+    count = 0
+    for x in palavra:
+        if x == letra:
+            count += 1
+    return count
+    
+
 def is_anagram(first_string, second_string):
-    ...
+    if len(first_string) != len(second_string):
+        return False
+    for x in first_string:
+        if contar(x, first_string) != contar(x, second_string):
+            return False
+    return True
